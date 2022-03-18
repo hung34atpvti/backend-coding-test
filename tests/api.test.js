5acf1d6267d13971c08522df8133be58abf205c7
declan <manh.n@trustana.com>
2022-03-18 16:08:44 +0700

    2. [Implement Tooling] 4. test: increase code coverage to >=80%

--- tests/api.test.js
+++ tests/api.test.js
@@ -32,4 +32,159 @@ describe('API tests', () => {
         .expect(200, done);
     });
   });
+
+  //eslint-disable-next-line
+    describe('GET /rides', () => {
+    //eslint-disable-next-line
+        it('should return 200 OK', done => {
+      request(app)
+        .get('/rides')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+    });
+  });
+
+  //eslint-disable-next-line
+  describe('GET /rides/id', () => {
+    //eslint-disable-next-line
+    it('should return 200 OK', done => {
+      request(app)
+        .get('/rides/1')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+    });
+  });
+
+  //eslint-disable-next-line
+  describe('POST /rides', () => {
+    //eslint-disable-next-line
+    it('should return 200 ERROR relate to start latitude and longitude', done => {
+      const body = {
+        start_lat: '-91',
+        start_long: '-180',
+        end_lat: '90',
+        end_long: '180',
+        rider_name: 'testDriver',
+        driver_name: 'testRider',
+        driver_vehicle: 'testVehicle'
+      };
+      const expectResult = JSON.stringify({
+        error_code: 'VALIDATION_ERROR',
+        message:
+          'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+      });
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, expectResult, done);
+    });
+
+    //eslint-disable-next-line
+    it('should return 200 ERROR relate to end latitude and longitude ', done => {
+      const body = {
+        start_lat: '-90',
+        start_long: '-180',
+        end_lat: '91',
+        end_long: '180',
+        rider_name: 'testDriver',
+        driver_name: 'testRider',
+        driver_vehicle: 'testVehicle'
+      };
+      const expectResult = JSON.stringify({
+        error_code: 'VALIDATION_ERROR',
+        message:
+          'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+      });
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, expectResult, done);
+    });
+
+    //eslint-disable-next-line
+    it('should return 200 ERROR relate to riderName', done => {
+      const body = {
+        start_lat: '-90',
+        start_long: '-180',
+        end_lat: '90',
+        end_long: '180',
+        rider_name: '',
+        driver_name: 'testRider',
+        driver_vehicle: 'testVehicle'
+      };
+      const expectResult = JSON.stringify({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Rider name must be a non empty string'
+      });
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, expectResult, done);
+    });
+
+    //eslint-disable-next-line
+    it('should return 200 ERROR relate to driverName', done => {
+      const body = {
+        start_lat: '-90',
+        start_long: '-180',
+        end_lat: '90',
+        end_long: '180',
+        rider_name: 'testRider',
+        driver_name: '',
+        driver_vehicle: 'testVehicle'
+      };
+      const expectResult = JSON.stringify({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Rider name must be a non empty string'
+      });
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, expectResult, done);
+    });
+
+    //eslint-disable-next-line
+    it('should return 200 ERROR relate to driverVehicle', done => {
+      const body = {
+        start_lat: '-90',
+        start_long: '-180',
+        end_lat: '90',
+        end_long: '180',
+        rider_name: 'testRider',
+        driver_name: 'testRider',
+        driver_vehicle: ''
+      };
+      const expectResult = JSON.stringify({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Rider name must be a non empty string'
+      });
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, expectResult, done);
+    });
+
+    //eslint-disable-next-line
+    it('should return 200 OK', done => {
+      const body = {
+        start_lat: '-90',
+        start_long: '-180',
+        end_lat: '90',
+        end_long: '180',
+        rider_name: 'testRider',
+        driver_name: 'testRider',
+        driver_vehicle: 'testVehicle'
+      };
+      request(app)
+        .post('/rides')
+        .send(body)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+    });
+  });
 });
